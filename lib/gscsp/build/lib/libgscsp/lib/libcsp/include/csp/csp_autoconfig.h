8b65cad516dd0eb1c096a34c1537ea7969668b3c
jane0828 <janectl29@gmail.com>
2026-03-14 01:16:00 +0900

    0314

--- lib/gscsp/build/lib/libgscsp/lib/libcsp/include/csp/csp_autoconfig.h
+++ lib/gscsp/build/lib/libgscsp/lib/libcsp/include/csp/csp_autoconfig.h
@@ -3,7 +3,7 @@
 #ifndef W_INCLUDE_CSP_CSP_AUTOCONFIG_H_WAF
 #define W_INCLUDE_CSP_CSP_AUTOCONFIG_H_WAF
 
-#define GIT_REV "d234e583"
+#define GIT_REV "a49b7f03"
 /* #undef CSP_FREERTOS */
 #define CSP_POSIX 1
 /* #undef CSP_WINDOWS */
